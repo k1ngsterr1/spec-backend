af058fff58c22c5130637a548a66831044794023
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-03-11 17:25:59 +0500

    fix: admin

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
 import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
