749cc5955209967b120868acae0121bbd4c31ccd
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-03-15 21:15:58 +0500

    fix

--- src/application-text/application-text.module.ts
+++ src/application-text/application-text.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApplicationTextService } from './application-text.service';
 import { ApplicationTextController } from './application-text.controller';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
 
 @Module({
   controllers: [ApplicationTextController],
-  providers: [ApplicationTextService],
+  providers: [ApplicationTextService, PrismaService],
 })
 export class ApplicationTextModule {}
