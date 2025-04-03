e9cc58eb2be6a8207498ec2c44beff0b16fd672a
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-04-03 20:50:45 +0500

    fix

--- src/forms/forms.module.ts
+++ src/forms/forms.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FormsService } from './forms.service';
 import { FormsController } from './forms.controller';
+import { SharedModule } from 'src/shared/shared.module';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
 
 @Module({
+  imports: [SharedModule],
   controllers: [FormsController],
-  providers: [FormsService],
+  providers: [FormsService, PrismaService],
 })
 export class FormsModule {}
