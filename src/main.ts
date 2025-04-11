2ebdfd1061c80ae46ae98348793d0b4d696a885c
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-04-11 11:54:22 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as path from 'path';
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   });
-
-  app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
   await app.listen(process.env.PORT ?? 6001);
 }
