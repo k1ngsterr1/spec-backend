f6c49c6414bc2374b97ba239cd2fa4ecc14dd1c4
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-04-11 11:45:23 +0500

    fix: fix api

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   app.enableCors({
     origin: 'http://localhost:5173',
