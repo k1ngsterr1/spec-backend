c219b7dbaf49dd5a26906464f76f7b6c0d568edd
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-04 18:41:06 +0500

    fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,12 @@
 import admin from 'firebase-admin';
-const serviceAccount = require('../serviceAccountKey.json');
+import * as fs from 'fs';
+import * as path from 'path';
+
+const serviceAccountPath = path.resolve(
+  process.cwd(),
+  'serviceAccountKey.json',
+);
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
